b673f5769ae0aa885fb8b89bb035e8329b1d5ec6
Martin Kopecký <kopecky@thekrew.app>
2023-10-27 09:42:02 +0200

    Zscore

--- Main.py
+++ Main.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pandas as pd
-from scipy.stats import variation 
+import scipy as sc
 from plotnine.data import economics
 from plotnine import *
 
-# Coefficient = variation
+# Coefficient = sc.stats.variation
+# Z score = sc.stats.zscore
 # Standard deviation = np.std
 # Variance = np.var
 
@@ -15,12 +16,16 @@ df = pd.read_csv("prij.csv")
 SS3 = np.std(np.array(df['ss3']))
 SS2 = np.std(np.array(df['ss2']))
 
+Z =  sc.stats.zscore(df['ss3'])
+
+
 print(SS3)
 print(SS2)
 
 print(SS3 > SS2)
 print(SS3 < SS2)
 
+print(Z)
 
 # GGplot rendering
 a = (ggplot(df, aes("Obor","celprij")))+ geom_point()
